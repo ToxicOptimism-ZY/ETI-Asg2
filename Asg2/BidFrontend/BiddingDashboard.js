708db41ff55417cc47482ec6cdf016b4c6741ece
Zhao Yi Yap <s10196983@connect.np.edu.sg>
2022-02-01 00:39:08 +0800

    Added status usage and function for get sem start

--- Asg2/BidFrontend/BiddingDashboard.js
+++ Asg2/BidFrontend/BiddingDashboard.js
@@ -1,16 +1,51 @@
 //==================== Structures & Variables ====================
-
-//==================== API Callers ====================
 const tokenURL = "http://10.31.11.12:9071/api/v1/Token"
 const classURL = "http://10.31.11.12:9101/api/v1/class"
 const bidURL = "http://10.31.11.12:9221/api/v1/bids"
 const key = "2c78afaf-97da-4816-bbee-9ad239abb298"
+
+//==================== Auxiliary Functions ====================
+// Compute the current semester's start date. Saturdays and Sundays will use the next semester's start date.
+function getSemesterStartDate() {
+    date = new Date();
+    if (date.getDay() <= 5) {
+        amount = date.getDay() - 1
+        date.setDate(date.getDate() - amount)
+    }
+    else if (date.getDay() >= 6) {
+        amount = 8 - date.getDay()
+        date.setDate(date.getDate() + amount)
+    }
+
+    return `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`
+}
+
+//==================== API Callers ====================
 
 //==================== Class API Callers ====================
 
 
 
 //==================== Bidding API Callers ====================
+function CreateBidRecord(){
+    url = bidURL + "?key=" + key;
+    $.ajax({
+        type: "POST",
+        url: url,
+        statusCode: {
+            401: function(response) {
+                console.log(response.responseText)
+            },
+            422: function(response) {
+                console.log(response.responseText)
+            },
+            201: function(response) {  
+                console.log(response.responseText)
+            },
+        }
+    });
+}
+
 function GetBidRecordByBidID(bidID){
     url = bidURL + "/" + bidID + "?key=" + key;
     $.ajax({
@@ -19,32 +54,125 @@ function GetBidRecordByBidID(bidID){
         success: function (response, _) {
             bid = JSON.parse(response);
         },
-        error: function (_, _, response) {
-            console.log(response);
-            bid = null;
+        statusCode: {
+            401: function(response) {
+                console.log(response.responseText)
+                bid = null;
+            },
+            404: function(response) {
+                console.log(response.responseText)
+                bid = null;
+            },
         }
     });
     return bid
 }
 
-GetSemesterBidRecordsByStatus
-GetStudentBidRecords
-GetStudentBidRecordByStatus
+function UpdateBidRecord(bidID){
+    url = bidURL + "/" + bidID + "?key=" + key;
+    $.ajax({
+        type: "PUT",
+        url: url,
+        statusCode: {
+            401: function(response) {
+                console.log(response.responseText)
+            },
+            404: function(response) {
+                console.log(response.responseText)
+            },
+            422: function(response) {
+                console.log(response.responseText)
+            },
+            202: function(response) {
+                console.log(response.responseText)
+            },
+        }
+    });
+}
 
-function GetStudentBidRecordForStatus(studentID, semesterStartDate, bidStatus){
-    url = bidURL + "?key=" + key  + "&studentID=" + studentID + "&classID=" + classID + "&semesterStartDate=" + semesterStartDate;
+function DeleteBidRecord(bidID){
+    url = bidURL + "/" + bidID + "?key=" + key;
+    $.ajax({
+        type: "GET",
+        url: url,
+        statusCode: {
+            401: function(response) {
+                console.log(response.responseText)
+            },
+            404: function(response) {
+                console.log(response.responseText)
+            },
+            202: function(response) {
+                console.log(response.responseText)
+            },
+        }
+    });
+}
+
+function GetSemesterBidRecordsByStatus(semesterStartDate, bidStatus){
+    url = bidURL + "?key=" + key  + "&semesterStartDate=" + semesterStartDate + "&status=" + bidStatus;
     $.ajax({
         type: "GET",
         url: url,
         success: function (response, _) {
-            bid = JSON.parse(response);
+            bids = JSON.parse(response);
         },
-        error: function (_, _, response) {
-            console.log(response);
-            bid = null;
+        statusCode: {
+            401: function(response) {
+                console.log(response.responseText)
+                bids = []
+            },
+            404: function(response) {
+                console.log(response.responseText)
+                bids = []
+            },
         }
     });
-    return bid
+    return bids
+}
+
+function GetStudentBidRecords(studentID, semesterStartDate){
+    url = bidURL + "?key=" + key  + "&studentID=" + studentID + "&semesterStartDate=" + semesterStartDate;
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (response, _) {
+            bids = JSON.parse(response);
+        },
+        statusCode: {
+            401: function(response) {
+                console.log(response.responseText)
+                bids = []
+            },
+            404: function(response) {
+                console.log(response.responseText)
+                bids = []
+            },
+        }
+    });
+    return bids
+}
+
+function GetStudentBidRecordByStatus(studentID, semesterStartDate, bidStatus){
+    url = bidURL + "?key=" + key  + "&studentID=" + studentID + "&semesterStartDate=" + semesterStartDate + "&status=" + bidStatus;
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (response, _) {
+            bids = JSON.parse(response);
+        },
+        statusCode: {
+            401: function(response) {
+                console.log(response.responseText)
+                bids = []
+            },
+            404: function(response) {
+                console.log(response.responseText)
+                bids = []
+            },
+        }
+    });
+    return bids
 }
 
 function GetStudentBidRecordForClass(studentID, classID, semesterStartDate){
@@ -55,9 +183,15 @@ function GetStudentBidRecordForClass(studentID, classID, semesterStartDate){
         success: function (response, _) {
             bid = JSON.parse(response);
         },
-        error: function (_, _, response) {
-            console.log(response);
-            bid = null;
+        statusCode: {
+            401: function(response) {
+                console.log(response.responseText)
+                bid = null
+            },
+            404: function(response) {
+                console.log(response.responseText)
+                bid = null
+            },
         }
     });
     return bid
@@ -71,9 +205,15 @@ function GetTopClassBidRecords(classID, semesterStartDate){
         success: function (response, _) {
             bids = JSON.parse(response);
         },
-        error: function (_, _, response) {
-            console.log(response);
-            bids = [];
+        statusCode: {
+            401: function(response) {
+                console.log(response.responseText)
+                bids = []
+            },
+            404: function(response) {
+                console.log(response.responseText)
+                bids = []
+            },
         }
     });
     return bids
@@ -87,10 +227,18 @@ function GetTopClassBidRecordsByStatus(classID, semesterStartDate, bidStatus){
         success: function (response, _) {
             bids = JSON.parse(response);
         },
-        error: function (_, _, response) {
-            bids = [];
+        statusCode: {
+            401: function(response) {
+                console.log(response.responseText)
+                bids = []
+            },
+            404: function(response) {
+                console.log(response.responseText)
+                bids = []
+            },
         }
     });
     return bids
 }
 
+//==================== JQuery ====================
