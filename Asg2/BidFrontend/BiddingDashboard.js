58c4ea628121bdb95c06729195472833eee125ee
Zhao Yi Yap <s10196983@connect.np.edu.sg>
2022-02-03 21:06:11 +0800

    Working on html and js, WIP

--- Asg2/BidFrontend/BiddingDashboard.js
+++ Asg2/BidFrontend/BiddingDashboard.js
@@ -1,5 +1,4 @@
 //==================== Structures & Variables ====================
-const tokenURL = "http://10.31.11.12:9071/api/v1/Token"
 const classURL = "http://10.31.11.12:9101/api/v1/class"
 const bidURL = "http://10.31.11.12:9221/api/v1/bids"
 const key = "2c78afaf-97da-4816-bbee-9ad239abb298"
@@ -20,30 +19,64 @@ function getSemesterStartDate() {
     return `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`
 }
 
+// Return a formatted string
+String.prototype.format = String.prototype.f = function() {
+    var s = this,
+        i = arguments.length;
+
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+};
+
 //==================== API Callers ====================
 
 //==================== Class API Callers ====================
-
-
+function GetClasses(){
+    url = classURL + "/" + "?key=" + key;
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (response, _) {
+            classes = JSON.parse(response);
+            errMsg = ""
+        },
+        statusCode: {
+            401: function(response) {
+                errMsg = response.responseText
+                classes = null;
+            },
+            404: function(response) {
+                errMsg = response.responseText
+                classes = null;
+            },
+        }
+    });
+    return errMsg, classes
+}
 
 //==================== Bidding API Callers ====================
-function CreateBidRecord(){
+function CreateBidRecord(jsonString){
     url = bidURL + "?key=" + key;
     $.ajax({
         type: "POST",
         url: url,
+        data: jsonString,
+        contentType: "application/json",
         statusCode: {
             401: function(response) {
-                console.log(response.responseText)
+                errMsg = response.responseText
             },
             422: function(response) {
-                console.log(response.responseText)
+                errMsg = response.responseText
             },
             201: function(response) {  
-                console.log(response.responseText)
+                errMsg = response.responseText
             },
         }
     });
+    return errMsg
 }
 
 function GetBidRecordByBidID(bidID){
@@ -56,57 +89,63 @@ function GetBidRecordByBidID(bidID){
         },
         statusCode: {
             401: function(response) {
-                console.log(response.responseText)
+                errMsg = response.responseText
                 bid = null;
             },
             404: function(response) {
-                console.log(response.responseText)
+                errMsg = response.responseText
                 bid = null;
             },
         }
     });
-    return bid
+    return errMsg,bid
 }
 
-function UpdateBidRecord(bidID){
+function UpdateBidRecord(bidID, jsonString){
     url = bidURL + "/" + bidID + "?key=" + key;
     $.ajax({
         type: "PUT",
         url: url,
+        data: jsonString,
+        contentType: "application/json",
         statusCode: {
             401: function(response) {
-                console.log(response.responseText)
+                errMsg = response.responseText
             },
             404: function(response) {
-                console.log(response.responseText)
+                errMsg = response.responseText
             },
             422: function(response) {
-                console.log(response.responseText)
+                errMsg = response.responseText
             },
             202: function(response) {
-                console.log(response.responseText)
+                errMsg = response.responseText
             },
         }
     });
+    return errMsg
 }
 
-function DeleteBidRecord(bidID){
+function DeleteBidRecord(bidID, jsonString){
     url = bidURL + "/" + bidID + "?key=" + key;
     $.ajax({
         type: "GET",
         url: url,
+        data: jsonString,
+        contentType: "application/json",
         statusCode: {
             401: function(response) {
-                console.log(response.responseText)
+                errMsg = response.responseText
             },
             404: function(response) {
-                console.log(response.responseText)
+                errMsg = response.responseText
             },
             202: function(response) {
-                console.log(response.responseText)
+                errMsg = response.responseText
             },
         }
     });
+    return errMsg
 }
 
 function GetSemesterBidRecordsByStatus(semesterStartDate, bidStatus){
@@ -241,4 +280,77 @@ function GetTopClassBidRecordsByStatus(classID, semesterStartDate, bidStatus){
     return bids
 }
 
-//==================== JQuery ====================
+//==================== Create JSON String ====================
+function createBid(semesterStartDate, classID, studentID, studentName, tokenAmount, status) {
+    const obj = JSON.parse(sampleString);
+    obj.bidID = parseInt(obj.CommentID);
+    obj.semesterStartDate = parseInt(obj.TargetID);
+    obj.classID = parseInt(obj.CreatorID);
+    obj.studentID = parseInt(obj.Anonymous);
+    const jsonString2 = JSON.stringify(obj);
+}
+
+//==================== Templates ====================
+sampleJsonString = '{ bidid: 0, semesterstartdate: "", classid: 0, studentid: "", studentname: "", tokenamount: 0, status: "Pending"}'
+sampleClassItem = `
+<div class='row' style='padding:40px 80px 40px 80px;' >
+    <div class='col-sm-12 fontstyle dataTable'>
+        <table style='border-collapse: collapse; width:100%;'  >
+            <thead>
+                <tr>
+                    <td style='text-align:left;'>
+                        <b id='classTitle'>{0} - {1}</b> 
+                    </td>
+                    <td colspan="2" style="text-align:right;">
+                        {2} Stars
+                    </td>
+                    <td style='text-align: right;'>
+                        Your Bid:    {3}
+                    </td>
+                </tr>           
+            </thead>
+            <tr>
+                <td>{4}, {5} - {6}</td>
+                <td colspan="2" style='text-align: right;'>Tutor: {7}</td>
+            </tr>
+            <tr>
+                <td colspan="3"> {8} </td>
+                <td style='text-align: right;'><a href='INSERT LINK HERE'><button class='viewDetails' classid='{9}' semesterstartdate='{10}' style='color:white; outline:none; border:none;'>View Details</button></a></td>
+            </tr>
+        </table>
+    </div>
+</div>`;
+
+//==================== JavaScript ====================
+function listAllClasses() {
+    classes = JSON.parse(GetClasses())
+    htmlString = ""
+
+    // check if input vs semesterStartDate, check if valid
+    semesterStartDate = currentSemesterStartDate 
+
+    for (var i = 0; i < classes.length; i++) {
+        htmlString += sampleClassItem.f(classes[i].modulecode,classes[i].classcode,classes[i].classrating,classes[i].tutorname,classes[i].classinfo,classes[i].classcode)
+    }
+
+    document.getElementById('scrollList').innerHTML = htmlString
+}
+
+function listClassBids() {
+
+}
+
+function listMyBids() {
+
+}
+
+function updateBid() {
+    bid = JSON.parse(GetBidRecordByBidID(bidID))
+}
+
+function deleteBid() {
+
+}
+
+
+currentSemesterStartDate = getSemesterStartDate()
